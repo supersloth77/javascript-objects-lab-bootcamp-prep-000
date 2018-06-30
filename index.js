bc12dfac6a83d2c38e37f35a294721b2a417f8f9
Peter Traversa <peter.traversa@gmail.com>
2018-06-30 19:36:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 var recipes = { margarita: "limes" };
 
-function updateObjectWithKeyAndValue(recipes, key, value) {
-  
-}
-
-function destructivelyUpdateObjectWithKeyAndVluae(recipes, key, value) {
-  
-}
-
-function deleteFromObjectByKey(recipes, key) {
-  
-}
-
-function destructivelyDeleteFromObjectByKey(recipes, key) {
-  
-}
