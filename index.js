fe58e18388fbae4954225b8a55107bf9c3310c57
Peter Traversa <peter.traversa@gmail.com>
2018-06-30 19:53:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function deleteFromObjectByKey(object, key) {
+  var newObject = object;
+  delete newObject[key];
+  return newObject;
+}
