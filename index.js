3d6fddfcd002450178128873588258d43a3c43b3
Peter Traversa <peter.traversa@gmail.com>
2018-06-30 19:41:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = { margarita: "limes" };
 
 function updateObjectWithKeyAndValue(recipes, key, value) {
-  var newObject = (recipes, { key: "value" });
+  var newObject = (recipes, { [key]: "value" });
   return newObject;
 }
